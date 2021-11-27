2a6a3ba4b1c7a510c7223885e984965d743483bf
Marva Athatillah <ahnafmarva@gmail.com>
2021-11-27 14:20:42 +0700

    Data (name + img) Bangun Datar

--- app/src/main/java/com/example/myapplication/data_list/GeocountData.kt
+++ app/src/main/java/com/example/myapplication/data_list/GeocountData.kt
@@ -1,5 +1,32 @@
 package com.example.myapplication.data_list
 
+import com.example.myapplication.R
+
 object GeocountData {
 
+    private val namaBangunDatar = arrayOf(
+        "Triangle",
+        "Circle",
+        "Rectangular",
+        "Rectangle",
+        "Trapezoid",
+        "Parallelogram",
+        "Cut the rice cake",
+        "Kite",
+        "Hexagon",
+        "Pentagon"
+    )
+
+    private val imgBangunDatar = intArrayOf(
+        R.drawable.segitiga,
+        R.drawable.lingkaran,
+        R.drawable.persegi,
+        R.drawable.persegipanjang,
+        R.drawable.trapesium,
+        R.drawable.jajargenjang,
+        R.drawable.belahketupat,
+        R.drawable.layanglayang,
+        R.drawable.segienam,
+        R.drawable.segilima
+    )
 }
