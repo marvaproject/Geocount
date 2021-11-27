bab5a72228bef18da6b28b5f4956d729a7e3f3e0
Marva Athatillah <ahnafmarva@gmail.com>
2021-11-27 13:48:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/data_list/Geocount.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.data_list
+
+import com.example.myapplication.R
+
+class Geocount {
+
+    private val namaBentuk = arrayOf(
+        "Triangle",
+        "Circle",
+
+    )
+
+    private val imgBentuk = intArrayOf(
+        R.drawable.segitiga,
+
+    )
+}
---
+++ app/src/main/java/com/example/myapplication/data_list/GeocountData.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.data_list
+
+object GeocountData {
+
+}
